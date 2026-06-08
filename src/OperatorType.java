061160988d40c47a89e60466d3f6518764a0c63e
yulim <iojl1202@naver.com>
2026-06-08 15:48:02 +0900

    feat: OperatorType Enum에 연산자 확인 메소드

--- src/OperatorType.java
+++ src/OperatorType.java
@@ -4,7 +4,18 @@ public enum OperatorType {
     MULTIPLY('*'),
     DIVIDE('/');
 
+    private final char symbol;
 
-    OperatorType(char c) {
+    OperatorType(char symbol) {
+        this.symbol = symbol;
+    }
+
+    public static OperatorType fromSymbol(char symbol) {
+        for(OperatorType o : OperatorType.values()) {
+            if(o.symbol == symbol) {
+                return o;
+            }
+        }
+        throw new IllegalArgumentException("잘못된 연산 기호입니다. ");
     }
 }
